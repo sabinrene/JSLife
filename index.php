3311926903bc6371b70a582ef7add8423e8f910c
SabinRene <Julian.orozco@protonmail.com>
2020-05-03 23:56:55 -0500

    1stUp

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<title>ITO investments</title>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" type="text/css" href="CSS/ITO.css">
+
+<link rel="stylesheet" type="text/css" href="CSS/Menu.css">
+<link rel="stylesheet" type="text/css" href="CSS/queries.css">
+<link rel="stylesheet" type="text/css" href="CSS/grid.css">
+
+<link rel="stylesheet" type="text/css" href="CSS/Footer.css">
+</head>
+
+
+
+<body>
+
+<?php include 'Menu.php'; ?>
+
+
+
+
+	<div class="Banner">
+
+		<div class="Headings">
+			<h2>Firma pionera en gestión </br>
+			    de capital privado</br>
+			<span class="BTxt2">en la región andina</span> </h2>
+		</div>
+
+	</div>
+
+
+
+<?php include 'Footer.php'; ?>
+
+</body>
+</html>
