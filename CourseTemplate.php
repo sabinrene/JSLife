c156298c802709dcb5e8e6bc0819b62eb6490045
SabinRene <Julian.orozco@protonmail.com>
2020-05-06 07:51:59 -0500

    Right Panel

--- CourseTemplate.php
+++ CourseTemplate.php
@@ -75,11 +75,45 @@
 					</div> <!-- END LearnCard -->
 
 
-					<div class="Reqs">
-						<h2> Requirements </h2>
-					</div>
+						<div class="Reqs">
+							<h2> Requirements </h2>
+								<ul>
+								  <li> No prior technical experience is required! All you need a computer! </li>
+								  <li> Can Create and Delete at will.. </li>
+								  <li> Coca Cola as Height are both adjustable things </li>
+								</ul>
+						</div>
 
 
+
+						<div class="Desc">
+							<h2> Description </h2>
+							<h3> Learn how to use SQL quickly and effectively with this course! </h3>
+							<p> You'll learn how to read and write complex queries to a database using one of
+								the most in demand skills - PostgreSQL. These skills are also applicable to any
+								other major SQL database, such as MySQL, Microsoft SQL Server, Amazon Redshift,
+								Oracle, and much more. </p>
+
+							<p> Learning SQL is one of the fastest ways to improve your career prospects as it
+								is one of the most in demand tech skills! In this course you'll learn quickly and
+								receive challenges and tests along the way to improve your understanding!</p>
+
+								<ul>
+									<li> Get started with PostgreSQL and PgAdmin , two of the world's most popular SQL tools </li>
+								  <li> Running advanced queries with string operations and comparison operations </li>
+									<li> No prior technical experience is required! All you need a computer! </li>
+								  <li> Can Create and Delete at will.. </li>
+								  <li> Coca Cola as Height are both adjustable things </li>
+								</ul>
+
+								<p> I've spent years as an instructor both online and in-person at Fortune 500 companies,
+									and this course is built to combine the best of both worlds, allowing you to learn at your
+									own pace through an interactive environment. You will start with the basics and soon find
+									yourself working with advanced commands, dealing with timestamp data and variable character
+									information like a seasoned professional. </p>
+									<!-- Insert Paragraphs from Course Content -->
+
+						</div>
 
 
 			</div>
@@ -89,8 +123,24 @@
 			<div class="CourseTemp-Right">
 					<div class="Course-Pic">  </div>
 					<div class="Course-Price"> <h1> $9.99 </h1> </div>
-					<div class="Course-AddCart"> Add to Cart </div>
-					<div class="Course-Buy"> Buy Now </div>
+					<div class="Course-Btns">
+						<div class="Btn02">
+							Add to Cart
+						</div>
+						<div class="Btn02">
+							Buy Now
+						</div>
+					</div>
+
+					<div class="Course-includes">
+						<p>This course includes: </p>
+							<ul>
+								<li> 17 hours of on-demand video </li>
+								<li> 11 articles to deepen knowledge </li>
+								<li> Mobile friendly Access </li>
+								<li> Fulltime access </li>
+							</ul>
+					</div>
 			</div>
 			<!-- End RIGHT -->
 
--- Php-inserts/FinitoList.php
+++ Php-inserts/FinitoList.php
@@ -3,6 +3,5 @@
 					<ul>
 					  <li> <b>Example 1:</b> <a href="Js01.php">Make Text Appear </a></li>
   					<li> <b>Example 2:</b> <a href="CourseTemplate.php">Udemy - Course Template </a></li>
-
 					</ul>
 			</div>
